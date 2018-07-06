83382c36d7021137d7530f0c28a3dadf7619014d
suches <908202921@qq.com>
2018-07-06 16:06:29 +0800

    up

--- dist/json-traverser.js
+++ dist/json-traverser.js
@@ -5,17 +5,21 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = jsonTraverser;
 function jsonTraverser(json, cb) {
+    var datapath = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+
     var keys = Object.keys(json);
     keys.map(function (key) {
+        var nextPath = datapath.slice();
+        nextPath.push(key);
         var item = json[key];
         switch (Object.prototype.toString.call(item)) {
             case '[object Array]':
             case '[object Object]':
                 {
-                    jsonTraverser(item, cb);
+                    jsonTraverser(item, cb, nextPath.slice());
                     break;
                 }
         }
-        cb && cb(item, key, json);
+        cb && cb(item, key, json, nextPath);
     });
 }
--- src/json-traverser.js
+++ src/json-traverser.js
@@ -1,15 +1,17 @@
-export default function jsonTraverser( json, cb ){
+export default function jsonTraverser( json, cb, datapath = [] ){
     let keys = Object.keys( json );
     keys.map( ( key ) => {
+        let nextPath = datapath.slice();
+            nextPath.push( key );
         let item = json[key]
             ;
         switch( Object.prototype.toString.call( item ) ){
             case '[object Array]': 
             case '[object Object]': {
-                jsonTraverser( item, cb );
+                jsonTraverser( item, cb, nextPath.slice() );
                 break;
             }
         }
-        cb && cb( item, key, json );
+        cb && cb( item, key, json, nextPath );
     });
 }
